f03474d9447c44834440ad33bc4ad657e747b414
letuanvux <100011860+letuanvux@users.noreply.github.com>
2026-03-18 14:39:05 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,21 +27,14 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = applicationId + "-" + flutter.versionName + "-" + new Date().format("yyyyMMddHHmmss")
+        versionName = flutter.versionName
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.debug
-        }
-        // Thay đổi tên app trong thư mục rebuild\app\outputs\apk\release
-        applicationVariants.all { variant ->
-            variant.outputs.all { output ->
-                def newName = "${versionName}-${variant.name}.apk"
-                outputFileName = newName
-            }
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
